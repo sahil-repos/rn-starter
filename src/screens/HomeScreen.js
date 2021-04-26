a6579df19e09f0a3abb61293dd41067499d21cae
sahil-repos <sahil.ntc@gmail.com>
2021-04-27 02:04:19 +0530

    Destructuring

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Text, StyleSheet ,View, Button,TouchableOpacity} from "react-native";
 
-const HomeScreen = props => {
-  //console.log(props)
+const HomeScreen = ({navigation}) => {
+  //Destructuring, accessed only navigation prop from the props
   return (
     <View>
   <Text style={styles.text}>Changed222 text</Text>
   <Button 
   title="Goto Componenets demo"
-  onPress={()=>props.navigation.navigate('Components')}
+  onPress={()=>navigation.navigate('Components')}
   />
 <Button
 title="Go to List demo"
 onPress={()=>{navigation.navigate('List')}}
 />
 
-  <TouchableOpacity onPress={()=>{props.navigation.navigate('List')}}>
+  <TouchableOpacity onPress={()=>{navigation.navigate('List')}}>
     <Text>
       Go to TouchableOpacity test
     </Text>
